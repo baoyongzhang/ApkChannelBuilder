a9cdfada33da110e2031daaaeb7da5c4b9d380d9
baoyongzhang <baoyz@live.com>
2016-12-06 17:31:20 +0800

    Initial Commit

---
+++ ApkCahnnelBuilder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ApkCahnnelBuilder
+//
+//  Created by wukongbao on 2016/12/6.
+//  Copyright © 2016年 baoyz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ApkCahnnelBuilder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ApkCahnnelBuilder
+//
+//  Created by wukongbao on 2016/12/6.
+//  Copyright © 2016年 baoyz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
