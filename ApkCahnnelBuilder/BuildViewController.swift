15d22644ed92b4f623811f57709c541cc6c44d32
baoyongzhang <baoyz@live.com>
2016-12-08 15:54:22 +0800

    init commit

---
+++ ApkCahnnelBuilder/BuildViewController.swift
@@ -0,0 +1,100 @@
+//
+//  BuildViewController.swift
+//  ApkCahnnelBuilder
+//
+//  Created by wukongbao on 2016/12/7.
+//  Copyright © 2016年 baoyz. All rights reserved.
+//
+
+import Cocoa
+import ZipArchive
+
+class BuildViewController: NSViewController {
+    
+    @IBOutlet var logText: NSTextView!
+    @IBOutlet weak var doneButton: NSButton!
+    
+    public var apkPath: String!
+    public var outputPath: String!
+    public var channels: Array<String>!
+    public var channelPrefix: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        doneButton.isEnabled = false
+        
+        DispatchQueue.global(qos: .background).async {
+            self.process()
+        }
+        
+    }
+    
+    func process() {
+        // 创建临时目录
+        let tempDir: String = outputPath + "/temp"
+        print(outputPath)
+        
+        printLog(text: "Apk 开始解压")
+        
+        // 解压 apk 文件
+        SSZipArchive.unzipFile(atPath: apkPath, toDestination: tempDir)
+        
+        printLog(text: "Apk 解压完成")
+        
+        // 遍历渠道，在 META-INF 写入空文件
+        let metaDir: String = tempDir + "/META-INF"
+        for channel in channels {
+            printLog(text: "开始打包渠道：" + channel)
+            let channelFileName = channelPrefix + channel
+            // 创建渠道文件
+            createEmptyFile(dir: metaDir, filename: channelFileName)
+            // 压缩新的 apk 包
+            printLog(text: "开始压缩 Apk：" + channel + ".apk")
+            SSZipArchive.createZipFile(atPath: outputPath + "/" + channel + ".apk", withContentsOfDirectory: tempDir)
+            // 删除渠道文件
+            deleteFile(dir: metaDir, filename: channelFileName)
+            printLog(text: "打包完成：" + channel)
+        }
+        // 删除临时目录
+        deleteDir(dir: tempDir)
+        
+        printLog(text: "done.")
+        
+        DispatchQueue.main.async {
+            self.doneButton.isEnabled = true
+        }
+    }
+    
+    func printLog(text: String) {
+        print(text)
+        DispatchQueue.main.async {
+            self.logText.textStorage?.append(NSAttributedString(string: text + "\n"))
+        }
+    }
+    
+    func createEmptyFile(dir: String, filename: String) {
+        FileManager.default.createFile(atPath: dir + "/" + filename, contents: nil, attributes: nil)
+    }
+    
+    func deleteFile(dir: String, filename: String) {
+        do {
+            try FileManager.default.removeItem(atPath: dir + "/" + filename)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    func deleteDir(dir: String) {
+        do {
+            try FileManager.default.removeItem(atPath: dir)
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        self.presenting?.dismissViewController(self)
+    }
+} 
--- ApkCahnnelBuilder/ViewController.swift
+++ ApkCahnnelBuilder/ViewController.swift
@@ -10,10 +10,20 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var selectApk: NSButton!
+    @IBOutlet weak var pathLabel: NSTextField!
+    @IBOutlet weak var outputLabel: NSTextField!
+    @IBOutlet var channelsText: NSTextView!
+    @IBOutlet weak var channelPrefixText: NSTextField!
+    @IBOutlet weak var encryptionText: NSPopUpButton!
+    @IBOutlet weak var keyText: NSSecureTextField!
+    
+    var apkPath: String!
+    var outputPath: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override var representedObject: Any? {
@@ -23,5 +33,74 @@ class ViewController: NSViewController {
     }
 
 
+    // 选择 Apk 文件
+    @IBAction func openApkFile(sender: AnyObject) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowedFileTypes = ["apk"]
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = false
+        openPanel.canCreateDirectories = false
+        openPanel.canChooseFiles = true
+        openPanel.beginSheetModal(for: self.view.window!, completionHandler: {(result: Int) -> Void in
+            if NSFileHandlingPanelOKButton == result {
+                if let url = openPanel.url {
+                    self.pathLabel.stringValue = url.lastPathComponent
+                    self.apkPath = url.path
+                }
+            }
+        })
+    }
+    
+    // 选择输出目录
+    @IBAction func selectOutputDir(sender: AnyObject) {
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.beginSheetModal(for: self.view.window!, completionHandler: {(result: Int) -> Void in
+            if NSFileHandlingPanelOKButton == result {
+                if let url = openPanel.url {
+                    self.outputLabel.stringValue = url.path
+                    self.outputPath = url.path
+                }
+            }
+        })
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if "StartBuild" == identifier {
+            // 参数检查
+            if self.apkPath == nil {
+                showMessage(text: "请选择 Apk 文件")
+                return false
+            }
+            if self.outputPath == nil {
+                showMessage(text: "请选择输出目录")
+                return false
+            }
+            if self.channelPrefixText.stringValue.isEmpty {
+                showMessage(text: "请输入渠道名称前缀")
+                return false
+            }
+        }
+        return super.shouldPerformSegue(withIdentifier: identifier, sender: sender)
+    }
+    
+    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+        if let controller = segue.destinationController as? BuildViewController {
+            controller.apkPath = self.apkPath
+            controller.outputPath = self.outputPath
+            controller.channels = self.channelsText.string?.components(separatedBy: "\n")
+            controller.channelPrefix = self.channelPrefixText.stringValue
+        }
+    }
+    
+    func showMessage(text: String) {
+        let alert = NSAlert()
+        alert.messageText = text
+        alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
+    }
+    
 }
 
